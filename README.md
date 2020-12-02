7938da885a175b806928cec12a95038962cddf9c
blueJpg <2238288979@qq.com>
2020-12-02 23:40:46 +0800

    xxx

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - 编译安装 dwm
 - 重命名scripts为 ~/.dwm
 - 复制dwm.desktop文件到/usr/share/xsessions 目录里
--
+
 
 ## 补丁
 见[patch-list](patch-list)目录。
@@ -12,3 +12,5 @@
 ## reference
 - [Ubuntu DWM 安装配置文档](https://zhuanlan.zhihu.com/p/262067759)
 - [怎么拥有自己的 dwm](https://www.bilibili.com/video/BV1D5411Y7Ub?from=search&seid=5539774639519650142)
+- [linuxdabbler/personal-dot-files](https://github.com/linuxdabbler/personal-dot-files)
+- [DWM initial setup and config. Icons, Bar Scripts, Keybindings, and Theming](https://www.youtube.com/watch?v=zaRzOEoyR4s)
--- dwm/config.h
+++ dwm/config.h
@@ -2,12 +2,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 5;       /* snap pixel */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -19,13 +19,14 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_pink[]        = "#ff3399";
 static const unsigned int baralpha  = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_pink  },
+	[SchemeHid]  = { col_cyan,  col_gray1, col_pink  },
 };
 
 static const unsigned int alphas[][3]      = {
@@ -35,7 +36,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { ">_", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -44,10 +45,11 @@ static const Rule rules[] = {
 	 */
 	/* class                  instance                title       tags mask     isfloating   monitor */
 	{ "Gimp",                 "gimp",                 NULL,       0,            1,           -1 },
-    { "Google-chrome",        "google-chrome",        NULL,       1 << 8,       0,           -1 },
-    { "Zathura",             "zathura",               NULL,       1 << 7,       0,           -1 },
-	{ "Vlc",                  "vlc",                  NULL,       1 << 6,       0,           -1 },
-	{ "St",                   "st",                   NULL,       1 << 1,       0,           -1 },
+    // { "Google-chrome",        "google-chrome",        NULL,       1 << 8,       0,           -1 },
+    // { "Firefox",        "firefox",        NULL,       1 << 8,       0,           -1 },
+    // { "Zathura",             "zathura",               NULL,       1 << 7,       0,           -1 },
+	// { "Vlc",                  "vlc",                  NULL,       1 << 6,       0,           -1 },
+	// { "St",                   "st",                   NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -99,7 +101,6 @@ static Key keys[] = {
 	{ 0,                 XF86XK_AudioMute,     spawn,          {.v = voltogglescript } },
 	{ 0,           XF86XK_MonBrightnessUp,     spawn,          {.v = backlight_up } },
 	{ 0,           XF86XK_MonBrightnessDown,   spawn,          {.v = backlight_down } },
-	{ 0,                  XF86XK_ScreenSaver,  spawn,          {.v = screenshotcmd } },
 
 	{ MODKEY|ControlMask,           XK_n,      spawn,          {.v = network_manager } }, // 网络控制
 	{ MODKEY|ControlMask,           XK_s,      spawn,          {.v = suspend } }, // 待机
@@ -109,6 +110,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_p,     togglescratch,  {.v = scratchpadcmd } }, // 弹出一个临时窗口
+	{ MODKEY,                       XK_s,      spawn,          {.v = screenshotcmd } }, // 屏幕截图
 	{ MODKEY,                       XK_b,      togglebar,      {0} }, // 是否隐藏状态栏
 	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } }, // 不同窗口切换
 	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } }, // 不同窗口切换
