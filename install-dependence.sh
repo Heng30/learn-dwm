f34c96b5ccbb1a8a886c03b408ed85a8d1189c81
blueJpg <2238288979@qq.com>
2021-02-25 00:55:39 +0800

    xxx

--- install-dependence.sh
+++ install-dependence.sh
@@ -4,4 +4,4 @@
 sudo apt-get install suckless-tools libx11-dev libxft-dev libxinerama-dev \
     make gcc compton feh acpi acpitool flameshot numlockx brightnessctl \
     xinput xbacklight ranger slock xautolock farbfeld ttf-symbola \
-    xf86-input-synaptics
+    xf86-input-synaptics devhelp git vim neofetch nvim valgrind
